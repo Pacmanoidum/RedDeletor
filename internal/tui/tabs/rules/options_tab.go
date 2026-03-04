94652a3b8c09e070fe1e21a9caea5cdaff84ca61
Pacmanoidum <137086097+Pacmanoidum@users.noreply.github.com>
2026-03-04 16:27:09 +0300

    Update options_tab.go

--- internal/tui/tabs/rules/options_tab.go
+++ internal/tui/tabs/rules/options_tab.go
@@ -6,9 +6,9 @@ import (
 
 	tea "github.com/charmbracelet/bubbletea"
 	zone "github.com/lrstanley/bubblezone"
-	"github.com/pashkov256/deletor/internal/tui/interfaces"
-	"github.com/pashkov256/deletor/internal/tui/options"
-	"github.com/pashkov256/deletor/internal/tui/styles"
+	"github.com/Pacmanoidum/RedDeletor/internal/tui/interfaces"
+	"github.com/Pacmanoidum/RedDeletor/internal/tui/options"
+	"github.com/Pacmanoidum/RedDeletor/internal/tui/styles"
 )
 
 type OptionsTab struct {
